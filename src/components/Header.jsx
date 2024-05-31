32096f3b0457cde7952f0e0dca3b4348c974fe26
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2024-05-31 10:27:49 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
     const logoutuser = async () => {
         let token = localStorage.getItem("usersdatatoken");
         console.log(token);
-        const res = await fetch("http://localhost:3000/logout", {
+        const res = await fetch("https://backendofmam.onrender.com/logout", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
