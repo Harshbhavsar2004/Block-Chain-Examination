d98b3a73e71643de0fa667a5f3e53d2e8055447f
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2024-07-13 14:55:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy: {
-      '/api': 'https://block-chain-backend.onrender.com' // replace with your server address
-    },
-  },
   plugins: [react()],
 })
